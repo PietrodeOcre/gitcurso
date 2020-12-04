6e9c56b6ba0690a07ef61b3eb48db4a522340601
davruizc <niscastrillo@gmail.com>
2020-12-04 18:14:53 +0100

    prueba conflicto

--- src/curso/git/HolaMundo.java
+++ src/curso/git/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo 2");
 		System.out.println("otra linea");
-
+		System.out.println("conflicto");
 	}
 
 }
